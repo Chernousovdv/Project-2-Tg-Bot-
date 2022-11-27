4f5c74b2ee37ce42addfb4d9d223d73acd9a56b1
Chernousov Danila <113239558+Chernousovdv@users.noreply.github.com>
2022-11-27 21:03:03 +0300

    Create get_data.py

---
+++ get_data.py
@@ -0,0 +1,58 @@
+import requests
+import json
+from fake_useragent import UserAgent
+
+
+
+ua = UserAgent()
+
+def champions_list():
+    response = requests.get(url='https://op.gg/api/v1.0/internal/bypass/meta/champions?hl=en_US',
+                            headers={'user-agent': f'{ua.random}'}
+                            )
+    with open('result.json', 'w') as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    data = response.json()
+    champions = data.get('data')
+    return champions
+
+
+def collect_data(user_input):
+    response = requests.get(url='https://op.gg/api/v1.0/internal/bypass/meta/champions?hl=en_US',
+    headers={'user-agent': f'{ua.random}'}
+    )
+    with open('result.json', 'w') as file:
+        json.dump(response.json(), file, indent=4, ensure_ascii=False)
+    data = response.json()
+    champions = data.get('data')
+
+    tg_answer = {}
+    for i in champions:
+        if i.get('name') == user_input:
+            champion_name = i.get('name')
+            entips = i.get('enemy_tips')
+            alltips = i.get('ally_tips')
+            image = i.get('image_url')
+            passive = i.get('passive')
+            abilities = i.get('spells')
+            tg_answer = (
+                {
+                    'name': champion_name,
+                    'enemy_tips': entips,
+                    'ally_tips': alltips,
+                    'image_url': image,
+                    'passive': passive,
+                    'spells': abilities
+
+                }
+            )
+    return tg_answer
+    # with open('result.json', 'w') as file:
+    #     json.dump(tg_answer, file, indent=4, ensure_ascii=False)
+
+
+def main(user_input):
+    collect_data(user_input)
+
+if __name__ == '__main__':
+    main()
