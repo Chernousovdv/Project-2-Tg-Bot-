016381e25a2843288bff084cebe4fd7192451548
Chernousov Danila <113239558+Chernousovdv@users.noreply.github.com>
2022-12-02 00:39:54 +0300

    Update Globals.py

--- Globals.py
+++ Globals.py
@@ -1,13 +1,15 @@
-
 class Globals:
-    is_smth = True;
+    is_info = True;
     def is_active(self):
-        return self.is_smth
+        return self.is_info
     def change(self, top):
-        self.is_smth = top
+        self.is_info = top
+
 
 is_abilities = Globals()
 is_passive = Globals()
 is_ally_tips = Globals()
 is_enemy_tips = Globals()
 is_found = Globals()
+is_found.change(False)
+
